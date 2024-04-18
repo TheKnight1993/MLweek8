4d8d08b3d8679a4e483ce7a54b05a7f0efe5b1d7
Kevin <kevinvermaat@icloud.com>
2024-04-19 00:33:04 +0200

    Create main.py and import basic libraries

---
+++ main.py
@@ -0,0 +1,13 @@
+# main run file
+
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import scipy as sp
+import keras
+import sklearn.decomposition
+import sklearn.model_selection
+import sklearn.preprocessing
+import sklearn.linear_model
+import sklearn.metrics
